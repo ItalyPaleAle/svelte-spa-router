88ab9de019e6b2ce6cfa0a45d8d677d017fbb846
Alessandro Segala <ItalyPaleAle@users.noreply.github.com>
2019-05-13 19:21:18 -0700

    Made the example a bit more interesting

--- example/src/routes/Home.svelte
+++ example/src/routes/Home.svelte
@@ -1,2 +1,10 @@
 <h2>Home!</h2>
-<p>Welcome home!</p>
+<p>Welcome to this sample code!</p>
+<p>Things to try:</p>
+<ul>
+    <li>Navigate around with the links and buttons (notice how certain links become active depending on the path</li>
+    <li>Try pressing the browsers' back and forward buttons</li>
+    <li>The "Replace current page" button will change the page without adding a new item in the history stack (try pressing the back button!)</li>
+    <li>Manually change the URL's fragment/hash</li>
+    <li>Try refreshing the page</li>
+</ul>
