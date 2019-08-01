402256e7a4cb0d3bc3a6f62b359be44f0e4ced59
Alessandro Segala <ItalyPaleAle@users.noreply.github.com>
2019-07-31 18:28:36 -0700

    Can pass a regular expression as route path
    This requires using maps rather than objects.

--- example/src/routes.js
+++ example/src/routes.js
@@ -1,9 +1,10 @@
 import Home from './routes/Home.svelte'
 import Name from './routes/Name.svelte'
 import Wild from './routes/Wild.svelte'
+import Regex from './routes/Regex.svelte'
 import NotFound from './routes/NotFound.svelte'
 
-export default {
+/*export default {
     // Exact path
     '/': Home,
 
@@ -19,4 +20,28 @@ export default {
 
     // Catch-all, must be last
     '*': NotFound,
-}
+}*/
+
+const routes = new Map()
+
+// Exact path
+routes.set('/', Home)
+
+// Allow children to also signal link activation
+routes.set('/brand', Home)
+
+// Using named parameters, with last being optional
+routes.set('/hello/:first/:last?', Name)
+
+// Wildcard parameter
+routes.set('/wild', Wild)
+routes.set('/wild/*', Wild)
+
+// Regular expressions
+routes.set(/\/regex\/(.*?)/i, Regex)
+routes.set(/\/(pattern|match)/i, Regex)
+
+// Catch-all, must be last
+routes.set('*', NotFound)
+
+export default routes
---
+++ example/src/routes/Regex.svelte
@@ -0,0 +1,7 @@
+<h2 class="routetitle">Regex route</h2>
+
+<p>Match is: <code id="regexmatch">{JSON.stringify(params)}</code></p>
+
+<script>
+export let params = {}
+</script>
