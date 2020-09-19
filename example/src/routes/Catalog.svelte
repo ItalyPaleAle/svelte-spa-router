ceb545f2c461fd44c5fa89b6b2ca09f396792dfa
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2020-09-19 20:25:35 +0000

    Minor tweaks to comments

--- example/src/routes/Catalog.svelte
+++ example/src/routes/Catalog.svelte
@@ -4,10 +4,12 @@
 <a id="next" href use:link={`/catalog/${id + 1}`}>Next</a>
 
 <script>
-  import {link} from '../../../Router.svelte'
-  export let params
-  let id
-  $: if (params) {
-      id = parseInt(params.id, 10)
-  }
+// Import the link action
+// Normally, this would be import: `import {link} from 'svelte-spa-router'`
+import {link} from '../../../Router.svelte'
+export let params
+let id
+$: if (params) {
+    id = parseInt(params.id, 10)
+}
 </script>
